c339123bf434716669f401c8c1072ea227d87d4f
yesman <percev.yevgen@gmail.com>
2024-10-14 02:59:15 +0300

    removing ResourceFile.cs

--- AvaloniaResourceEditor/ViewModels/ResourceFile.cs
+++
@@ -1,8 +0,0 @@
-using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace AvaloniaResourceEditor.ViewModels;
-
-public partial class ResourceFile: ViewModelBase
-{
-    [ObservableProperty] private string _name = "new";
-}
